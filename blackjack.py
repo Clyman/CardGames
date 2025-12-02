837373b6b4e2c0a1b827ceba045a95f14c8306b0
Clyman <97504712+Clyman@users.noreply.github.com>
2025-12-02 17:08:09 +0800

    added new logic to the game

--- blackjack.py
+++ blackjack.py
@@ -28,6 +28,7 @@ def shuffle (x):
 def initialcardssingleplayer (playingdeck):
     dealer = []
     player = []
+    total = 0
     player.append(playingdeck.pop(0))
     dealer.append(playingdeck.pop(0))
     print("Dealing first card..")
@@ -43,8 +44,34 @@ def initialcardssingleplayer (playingdeck):
     print("Dealing second card....")
     time.sleep(1)
     #print(f"Dealer's Cards : {dealer}")
+    #Testing function
+    #player = ["10-Diamond", "Ace-Spade"]
     print(f"Your Cards : {player}")
-    return dealer, player, playingdeck
+    #Sum for total amount
+    laserace = 0
+    laserblackjack = 0
+    for i, card in enumerate(player):
+        try:
+            left = int(card.split("-")[0])
+            total = total + left
+            if left == 10:
+                laserblackjack = 1
+        except ValueError:
+            leftlaser = str(card.split("-")[0])
+            if leftlaser in {"Jack", "Queen", "King"}:
+                leftlaser = 10
+                total = total + leftlaser
+                laserblackjack = 1
+            elif leftlaser == "Ace":
+                leftlaser = 11
+                total = total + leftlaser
+                laserace = laserace + 1
+    if laserblackjack == laserace and laserblackjack != 0:
+        print("BLACKJACK! YOU WIN x2")
+    if laserace == 2:
+        print("BAN BAN! YOU WIN x3")
+    print(f"Your total number is : {total}")
+    return dealer, player, playingdeck, total
 
 def playerhit (playingdeck, player):
     player.append(playingdeck.pop(0))
@@ -56,12 +83,22 @@ def main():
     deckofcards = cardsindeck()
     shuffleddeck = shuffle(deckofcards)
     print(shuffleddeck)
-    #print(input("Do"))
-    dealer, player, playingdeck = initialcardssingleplayer(shuffleddeck)
-    playingdeck, player = playerhit(playingdeck, player)
-    print(len(playingdeck))
-
-
+    while True:
+        choice = input("Do you want to play some BlackJack? yes or no: ").strip().casefold()
+        if choice == "yes":
+            print("You chose yes. Get ready for some fun")
+            dealer, player, playingdeck, total = initialcardssingleplayer(shuffleddeck)
+            #playingdeck, player = playerhit(playingdeck, player)
+            print(len(playingdeck))
+            deckofcards = cardsindeck()
+            shuffleddeck = shuffle(deckofcards)
+            print(shuffleddeck)
+        if choice == "no":
+            print("Have a great day quitter :)")
+            break
+        else:
+            print("Please input only either yes or no")
+    
 if __name__ == "__main__":
     main()
 
