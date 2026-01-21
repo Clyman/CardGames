1712eac4e5208a9f2485bc2810113e128f8e951e
Clyman <clymanang@hotmail.com>
2026-01-21 20:28:23 +0800

    Remember to change code logic. Gameflow one class and others another

--- bacarrat.py
+++ bacarrat.py
@@ -1,4 +1,4 @@
-import random
+import random, math
 
 class Deck:
     def __init__(self):
@@ -16,14 +16,73 @@ class Deck:
     def shuffle(self):
         random.shuffle(self.cardlist)
 
-class Player:
-    def __init__(self, Name, cards):
+class Person:
+    def __init__(self, Name, cards = []):
         self.Name = Name
         self.cards = cards
+        self.total = 0
+        self.lastnumber = 0
+
+    def firstdraw(self):
+        firstcard = deckofcards.cardlist[0]
+        deckofcards.cardlist.pop(0)
+        self.cards.append(firstcard)
+        secondcard = deckofcards.cardlist[0]
+        deckofcards.cardlist.pop(0)
+        self.cards.append(secondcard)
+
+    def gettotalnumber(self):
+        for card in self.cards:
+            try:
+                left = int(card.split("-")[0])
+                self.total = self.total + left
+            except ValueError:
+                left = str(card.split("-")[0])
+                if left in {"Jack", "Queen", "King"}:
+                    self.total = self.total + 10
+                if left == "Ace":
+                    self.total = self.total + 1
+
+    def drawcard(self):
+        self.total = 0
+        lastcard = deckofcards.cardlist[0]
+        deckofcards.cardlist.pop(0)
+        self.cards.append(lastcard)
+
+    def getlastnumber(self):
+        self.lastnumber = self.total % 10
+
+
+class Player(Person):
+    def __init__(self, Name):
+        super().__init__(Name)
+
+class Dealer(Person)
+    def __init__(self, Name):
+        super().__init__(Name)
+
+class Engine:
+    def __init__(self, deck, player, dealer):
+
         
+
 deckofcards = Deck()
 print(deckofcards.cardlist)
 print(len(deckofcards.cardlist))
-
 deckofcards.shuffle()
 print(deckofcards.cardlist)
+p = Person("Clyman")
+print(p.Name)
+p.firstdraw()
+print(p.cards)
+p.gettotalnumber()
+print(p.total)
+p.getlastnumber()
+print(p.lastnumber)
+p.drawcard()
+p.gettotalnumber()
+print(p.cards)
+print(p.total)
+p.getlastnumber()
+print(p.lastnumber)
+
