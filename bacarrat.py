9d5160c1aaa110dd00e652dd281eda10462f49f1
Clyman <97504712+Clyman@users.noreply.github.com>
2026-01-22 20:53:56 +0800

    Changes

--- bacarrat.py
+++ bacarrat.py
@@ -1,4 +1,4 @@
-import random, math
+import random, math, time
 
 class Deck:
     def __init__(self):
@@ -22,6 +22,7 @@ class Person:
         self.lastnumber = 0
 
     def gettotalnumber(self):
+        self.total = 0
         for card in self.cards:
             try:
                 left = int(card.split("-")[0])
@@ -32,24 +33,24 @@ class Person:
                     self.total = self.total + 10
                 if left == "Ace":
                     self.total = self.total + 1
-
-    def drawcard(self):
-        self.total = 0
-        lastcard = deckofcards.cardlist[0]
-        deckofcards.cardlist.pop(0)
-        self.cards.append(lastcard)
-
-    def getlastnumber(self):
+        print(self.total)
         self.lastnumber = self.total % 10
+        print(self.lastnumber)
 
 
 class Player(Person):
     def __init__(self, Name):
         super().__init__(Name)
 
+    def playerwin(self):
+        print(f"Your final number is {self.player.lastnumber}. YOU WIN!!!!")
+
 class Dealer(Person):
     def __init__(self, Name):
         super().__init__(Name)
+
+    def dealerwin(self):
+        print(f"Django's final number is {self.dealer.lastnumber}. YOU LOSE!!!")
 
 class Engine:
     def __init__(self, deck, player, dealer):
@@ -64,25 +65,66 @@ class Engine:
         self.player.cards.append(self.deck[0])
         self.deck.pop(0)
         print(self.player.cards)
+        self.dealing()
         self.dealer.cards.append(self.deck[0])
         self.deck.pop(0)
-        print(self.dealer.cards)
+        #print(self.dealer.cards)
         self.player.cards.append(self.deck[0])
         self.deck.pop(0)
+        self.dealing()
         print(self.player.cards)
         self.dealer.cards.append(self.deck[0])
         self.deck.pop(0)
-        print(self.dealer.cards)
+        #print(self.dealer.cards)
     
+
+    def drawcard(self):
+        while True:
+            rubbercard = input(f"Do you want to draw a card? y for yes, n for no")
+            if rubbercard == "y":
+                self.total = 0
+                lastcard = self.deck[0]
+                self.deck.pop(0)
+                self.cards.append(lastcard)
+            
+            elif rubbercard == "n":
+                return
+            else:
+                print(f"Please input only either y or n")
+
+    def printcards(self):
+        print(self.cards)
+
+    def dealing(self):
+        time.sleep(2)
+
+
+    def play_round(self):
+        print("Shuffling Deck....")
+        self.dealing()
+        self.shuffledeck()
+        print("Drawing cards")
+        self.dealing()
+        self.firstdraw()
+        self.player.gettotalnumber()
+        self.dealer.gettotalnumber()
+        if self.player.lastnumber >= 8 and self.player.lastnumber > self.dealer.lastnumber:
+            self.player.playerwin()
+        if self.dealer.lastnumber >= 8 and self.dealer.lastnumber > self.player.lastnumber:
+            self.dealer.dealerwin()
+        if self.player.lastnumber == self.dealer.lastnumber:
+            print(f"It's a draw :((((")
+        else:
+            self.drawcard()
+
 
 
 deck = Deck()   
-p = Player("Dragoon rider")
+p = Player(input("What is your name?"))
+print(f"Welcome {p.Name}, my name is Django, get ready for some Bacarrat")
 d = Dealer("Django")
 game = Engine(deck.cardlist, p, d)
-game.shuffledeck()
-game.firstdraw()
-print(game.deck)
-print(len(game.deck))
+game.play_round()
+
 
 
