c7d47f4505a540c0d715dcca81ffd586e73541dd
Clyman <97504712+Clyman@users.noreply.github.com>
2026-01-28 14:59:12 +0800

    Changes to logic

--- bacarrat.py
+++ bacarrat.py
@@ -33,9 +33,10 @@ class Person:
                     self.total = self.total + 10
                 if left == "Ace":
                     self.total = self.total + 1
-        print(self.total)
         self.lastnumber = self.total % 10
-        print(self.lastnumber)
+
+    def printcards(self):
+        print(self.cards)
 
 
 class Player(Person):
@@ -43,14 +44,23 @@ class Player(Person):
         super().__init__(Name)
 
     def playerwin(self):
-        print(f"Your final number is {self.player.lastnumber}. YOU WIN!!!!")
+        print(f"Your Cards : {self.cards}")
+        print(f"Your final number is {self.lastnumber}. YOU WIN!!!!")
+
+    def printcards(self):
+        print(f"Your Cards : {self.cards}")
+
 
 class Dealer(Person):
     def __init__(self, Name):
         super().__init__(Name)
 
     def dealerwin(self):
-        print(f"Django's final number is {self.dealer.lastnumber}. YOU LOSE!!!")
+        print(f"Django's cards : {self.cards}")
+        print(f"Django's final number is {self.lastnumber}. YOU LOSE!!!")
+
+    def printcards(self):
+        print(f"Django's Cards : {self.cards}")
 
 class Engine:
     def __init__(self, deck, player, dealer):
@@ -64,7 +74,7 @@ class Engine:
     def firstdraw(self):
         self.player.cards.append(self.deck[0])
         self.deck.pop(0)
-        print(self.player.cards)
+        print(f"Your Cards : {self.player.cards}")
         self.dealing()
         self.dealer.cards.append(self.deck[0])
         self.deck.pop(0)
@@ -73,31 +83,79 @@ class Engine:
         self.deck.pop(0)
         self.dealing()
         print(self.player.cards)
+        self.player.gettotalnumber()
         self.dealer.cards.append(self.deck[0])
         self.deck.pop(0)
+        self.dealer.gettotalnumber()
         #print(self.dealer.cards)
+
+    def conclusion(self):
+        if self.player.lastnumber == self.dealer.lastnumber:
+            self.tie()
+            return
+        elif self.player.lastnumber > self.dealer.lastnumber:
+            self.player.playerwin()
+            return
+        elif self.player.lastnumber < self.dealer.lastnumber:
+            self.dealer.dealerwin()
+            return
     
 
     def drawcard(self):
         while True:
-            rubbercard = input(f"Do you want to draw a card? y for yes, n for no")
+            rubbercard = input(f"Do you want to draw a card? y for yes, n for no: ")
             if rubbercard == "y":
-                self.total = 0
+                self.player.total = 0
+                print("You draw one card.....")
+                self.dealing()
                 lastcard = self.deck[0]
                 self.deck.pop(0)
-                self.cards.append(lastcard)
-            
+                self.player.cards.append(lastcard)
+                self.player.printcards()
+                self.player.gettotalnumber()
+                if self.dealer.lastnumber < 5:
+                    self.dealer.total = 0
+                    dealerlastcard = self.deck[0]
+                    self.dealing()
+                    self.deck.pop(0)
+                    self.dealer.cards.append(dealerlastcard)
+                    self.player.printcards()
+                    self.player.gettotalnumber()
+                    self.dealer.printcards()
+                    self.dealer.gettotalnumber()
+                    self.conclusion()
+                    return
+                else:
+                    self.conclusion()
+                    return
+     
             elif rubbercard == "n":
-                return
+                if self.dealer.lastnumber < 5:
+                    self.dealer.total = 0
+                    dealerlastcard = self.deck[0]
+                    print(f"Dealer Django drawing card....")
+                    self.dealing()
+                    self.deck.pop(0)
+                    self.dealer.cards.append(dealerlastcard)
+                    self.player.printcards()
+                    self.player.gettotalnumber()
+                    self.dealer.printcards()
+                    self.dealer.gettotalnumber()
+                    self.conclusion()
+                    return
+                else:
+                    self.conclusion()
+                    return
+            
             else:
                 print(f"Please input only either y or n")
 
-    def printcards(self):
-        print(self.cards)
 
     def dealing(self):
-        time.sleep(2)
+        time.sleep(1.5)
 
+    def tie(self):
+        print(f"It's a draw :((((")
 
     def play_round(self):
         print("Shuffling Deck....")
@@ -106,22 +164,28 @@ class Engine:
         print("Drawing cards")
         self.dealing()
         self.firstdraw()
-        self.player.gettotalnumber()
-        self.dealer.gettotalnumber()
         if self.player.lastnumber >= 8 and self.player.lastnumber > self.dealer.lastnumber:
             self.player.playerwin()
+            return
         if self.dealer.lastnumber >= 8 and self.dealer.lastnumber > self.player.lastnumber:
             self.dealer.dealerwin()
-        if self.player.lastnumber == self.dealer.lastnumber:
-            print(f"It's a draw :((((")
+            return
+        if self.player.lastnumber >=8 and self.player.lastnumber == self.dealer.lastnumber:
+            self.tie()
+            return
         else:
             self.drawcard()
+
+        
+            
+        
 
 
 
 deck = Deck()   
 p = Player(input("What is your name?"))
-print(f"Welcome {p.Name}, my name is Django, get ready for some Bacarrat")
+d = Dealer("Django")
+print(f"Welcome {p.Name}, my name is {d.Name}, get ready for some Bacarrat")
 d = Dealer("Django")
 game = Engine(deck.cardlist, p, d)
 game.play_round()
