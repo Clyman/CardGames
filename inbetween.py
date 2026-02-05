86bec70fb27b69f0d1732b47615006c6495a9e9b
Clyman <97504712+Clyman@users.noreply.github.com>
2026-02-05 18:19:06 +0800

    Added logic

--- inbetween.py
+++ inbetween.py
@@ -24,9 +24,9 @@ class Person:
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
-        self.hand = []
+        self.hand = [None]
 
-    def drawcard(self, deck):
+    def draw_card(self, deck):
         self.hand[0] = deck.card_list[0]
         deck.card_list.pop(0)
 
@@ -34,27 +34,47 @@ class Person:
 class Table:
     def __init__(self):
         self.deck = Deck()
-        self.table_cards = []
+        self.table_cards = [None, None]
         self.players = []
+    
+
+    def next_two_cards(self):
+        self.table_cards[0] = self.deck.card_list[0]
+        self.deck.card_list.pop(0)
+        self.table_cards[1] = self.deck.card_list[0]
+        self.deck.card_list.pop(0)
+
+
+class Engine:
+    def __init__(self, table):
+        self.table = table
 
     def initialise_players(self):
         while True:
             try:
-                self.balance = int(input(f"Please enter the amount each person should have: "))
+                self.table.balance = int(input(f"Please enter the amount each person should have: "))
                 break
             except ValueError:
                 print(f"Please only input a whole number")
         while True:
             print("Please enter Player Name and type end once done")
             name = str(input(f"Player Name: "))
-            if name != "end":
-                self.players.append(Person(name, self.balance))
+            if name != "end" and name != "":
+                self.table.players.append(Person(name, self.table.balance))
             if name == "end":
                 break
+            elif name == "":
+                print("Please enter an appropriate value")
+                continue
             elif name == int or name == float:
                 print("Please enter an appropriate value")
                 continue
-    
+
+    def print_players(self):
+        print(f"Player List for this round of Bacarrat: ")
+        for i, name in enumerate(self.table.players):
+            print(f"{self.table.players[i].name}  ---  Balance : {self.table.players[i].balance}")
+
     def starting_to_prize_pool(self):
         while True:
             try:
@@ -64,32 +84,69 @@ class Table:
                 print(f"Please enter only a whole number!!!")
                 continue
 
-        no_of_players = len(self.players)
-        self.prize_pool = no_of_players * buy_in
-        print(f"Total amount in Prize Pool : {self.prize_pool}")
+        no_of_players = len(self.table.players)
+        self.table.prize_pool = no_of_players * buy_in
+        print(f"Total amount in Prize Pool : {self.table.prize_pool}")
 
-    def print_players(self):
-        print(f"Player List for this round of Bacarrat: ")
-        for i, name in enumerate(self.players):
-            print(f"{self.players[i].name}  ---  Balance : {self.players[i].balance}")
+    def table_two_cards(self):
+        confirmation = input(f"Press Enter for next two cards.")
+        while True:
+            if confirmation == "":
+                self.table.next_two_cards()
+                print(self.table.table_cards)
+                break
+            else:
+                print("Please only use Enter to continue")
+                continue
 
-    def next_two_cards(self, deck):
-        self.table_cards[0] = deck.card_list[0]
-        deck.card_list.pop(0)
-        self.table_cards[1] = deck.card_list[0]
-        deck.card_list.pop(0)
+    def compute_cards(self, index, bet):
+        RANK_VALUE = {
+            "Ace": 1,
+            "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+            "7": 7, "8": 8, "9": 9, "10": 10,
+            "Jack": 11, "Queen": 12, "King": 13,
+        }
+        table_first = RANK_VALUE[self.table.table_cards[0].split("-")[0]]
+        table_second = RANK_VALUE[self.table.table_cards[1].split("-")[0]]
+        player_first = RANK_VALUE[self.table.players[index].hand[0].split("-")[0]]
+        low, high = sorted((table_first, table_second))
+
+        if player_first == low or player_first == high:
+            print(f"OHHHHHH {self.table.players[index].name} TIANG TIANG TIANG. PAY DOUBLE %@#$^@!")
+            tiang = bet * 2
+            self.table.prize_pool = self.table.prize_pool + tiang
+            self.table.players[index].balance = self.table.players[index].balance - tiang
+        elif player_first > low and player_first < high:
+            print(f"{self.table.players[index].name} WINS!!!")
+            self.table.prize_pool = self.table.prize_pool - bet
+            self.table.players[index].balance = self.table.players[index].balance + bet 
+        
 
 
-class Engine:
-    def __init__(self, table):
-        self.table = table
-    
+
+
+    def player_turn(self, index):
+        print(f"It's {self.table.players[index].name}'s turn")
+        self.table_two_cards()
+        while True:
+            bet = input(f"How much would you like to bet? Input skip if you want to skip.")
+            if isinstance(bet, int):
+                double_confirm = input(f"Are you sure you want to bet this amount : {bet}. y for yes n for no")
+                if double_confirm == "y":
+                    self.table.players[index].draw_card()
+                    print(self.table.player[index].hand)
+                    
+        
     def play_round(self):
         self.table.deck.populate()
         self.table.deck.shuffle()
-        self.table.initialise_players()
-        self.table.print_players()
-        self.table.starting_to_prize_pool()
+        print(self.table.deck.card_list)
+        self.initialise_players()
+        self.print_players()
+        self.starting_to_prize_pool()
+        for index, name in enumerate(self.table.players):
+            self.player_turn(index)
+            self.table_two_cards()
 
 table = Table()
 game = Engine(table)
