8a399574601279ed13f9bca8563ccbf49f1cc278
Clyman <97504712+Clyman@users.noreply.github.com>
2026-02-06 17:42:45 +0800

    Added more logic

--- inbetween.py
+++ inbetween.py
@@ -7,6 +7,7 @@ class Deck:
         self.Suits = ["Diamond", "Clubs", "Hearts", "Spades"]
 
     def populate(self):
+        self.card_list = []
         for i in range(2, 11):
             for s in self.Suits:
                 card = f"{i}-{s}"
@@ -74,6 +75,10 @@ class Engine:
         print(f"Player List for this round of Bacarrat: ")
         for i, name in enumerate(self.table.players):
             print(f"{self.table.players[i].name}  ---  Balance : {self.table.players[i].balance}")
+        self.print_prize_pool()
+
+    def print_prize_pool(self):
+        print(f"The amount in the prize pool is left with {self.table.prize_pool}")
 
     def starting_to_prize_pool(self):
         while True:
@@ -86,6 +91,9 @@ class Engine:
 
         no_of_players = len(self.table.players)
         self.table.prize_pool = no_of_players * buy_in
+        for index, name in enumerate(self.table.players):
+            self.table.players[index].balance = self.table.players[index].balance - buy_in
+        self.print_players()
         print(f"Total amount in Prize Pool : {self.table.prize_pool}")
 
     def table_two_cards(self):
@@ -106,6 +114,7 @@ class Engine:
             "7": 7, "8": 8, "9": 9, "10": 10,
             "Jack": 11, "Queen": 12, "King": 13,
         }
+        next_index = (index + 1) % len(self.table.players)
         table_first = RANK_VALUE[self.table.table_cards[0].split("-")[0]]
         table_second = RANK_VALUE[self.table.table_cards[1].split("-")[0]]
         player_first = RANK_VALUE[self.table.players[index].hand[0].split("-")[0]]
@@ -116,11 +125,22 @@ class Engine:
             tiang = bet * 2
             self.table.prize_pool = self.table.prize_pool + tiang
             self.table.players[index].balance = self.table.players[index].balance - tiang
+            self.print_players()
+            print(f"{self.table.players[next_index].name}'s turn")
+            
         elif player_first > low and player_first < high:
             print(f"{self.table.players[index].name} WINS!!!")
             self.table.prize_pool = self.table.prize_pool - bet
             self.table.players[index].balance = self.table.players[index].balance + bet 
-        
+            self.print_players()
+            print(f"{self.table.players[next_index].name}'s turn")
+            
+        elif player_first < low or player_first > high:
+            print(f"{self.table.players[index].name} LOSES!!! PAY UP")
+            self.table.prize_pool = self.table.prize_pool + bet
+            self.table.players[index].balance = self.table.players[index].balance - bet
+            self.print_players()
+            print(f"{self.table.players[next_index].name}'s turn")
 
 
 
@@ -129,12 +149,30 @@ class Engine:
         print(f"It's {self.table.players[index].name}'s turn")
         self.table_two_cards()
         while True:
-            bet = input(f"How much would you like to bet? Input skip if you want to skip.")
-            if isinstance(bet, int):
-                double_confirm = input(f"Are you sure you want to bet this amount : {bet}. y for yes n for no")
-                if double_confirm == "y":
-                    self.table.players[index].draw_card()
-                    print(self.table.player[index].hand)
+            bet = input(f"How much would {self.table.players[index].name} like to bet? Input skip if you want to skip.").strip()
+            if bet.lower() == "skip":
+                print(f"{self.table.players[index].name} has chosen to skip. On to the next player")
+                self.print_players()
+                return
+            try:
+                bet = int(bet)
+            except ValueError:
+                print(f"Please input either an integer for bet or skip if you want to skip !!!!")
+                continue
+            if bet <= 0:
+                print("Please input a positive whole number for the bet.")
+                continue
+
+            double_confirm = input(f"Are you sure you want to bet this amount : {bet}? Enter again for yes n for no").strip().lower()
+            if double_confirm == "":
+                self.table.players[index].draw_card(self.table.deck)
+                print(self.table.players[index].hand)
+                self.compute_cards(index, bet)
+                return
+            if double_confirm == "n":
+                print(f"Please input your bet again {self.table.players[index].name}")
+                continue
+            print("Please type y or n.")
                     
         
     def play_round(self):
@@ -142,11 +180,24 @@ class Engine:
         self.table.deck.shuffle()
         print(self.table.deck.card_list)
         self.initialise_players()
-        self.print_players()
         self.starting_to_prize_pool()
-        for index, name in enumerate(self.table.players):
-            self.player_turn(index)
-            self.table_two_cards()
+        if len(self.table.players) == 0:
+            return
+
+        current_index = 0
+        del self.table.deck.card_list[:45]
+        while True:
+
+            if len(self.table.deck.card_list) >= 3:
+                print(self.table.deck.card_list)
+                self.player_turn(current_index)
+                current_index = (current_index + 1) % len(self.table.players)
+            else:
+                self.table.deck.populate()
+                self.table.deck.shuffle()
+                continue
+
+        
 
 table = Table()
 game = Engine(table)
--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-cardlist = []
-Ranks = ["Ace", "Jack", "Queen", "King"]
-Suits = ["Diamond", "Clubs", "Hearts", "Spades"]
-for i in range(2, 11):
-    for s in Suits:
-        card = f"{i}-{s}"
-        print(card)
+
+
